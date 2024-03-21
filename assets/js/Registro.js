54a24f19fe41e28fcffeccdc57fd1bd889b749dd
matheusAI <ms2279584@gmail.com>
2024-03-21 15:56:12 -0300

    test com o servido

--- assets/js/Registro.js
+++ assets/js/Registro.js
@@ -1,24 +1,24 @@
 function submitForm() {
-  const username = document.getElementById('username').value;
+  const nome = document.getElementById('username').value;
   const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const confirmPassword = document.getElementById('ConfimarPassword').value;
+  const senha = document.getElementById('password').value;
+  const confirmsenha = document.getElementById('ConfimarPassword').value;
 
   // Verifica se as senhas coincidem
-  if (password !== confirmPassword) {
+  if (senha !== confirmsenha) {
     alert('As senhas não coincidem');
     return;
   }
   // Envia dados para o back-end (usando o endpoint de registro do ReqRes)
-  fetch('https://reqres.in/api/register', {
+  fetch('http://172.21.16.1:3341/creteUser', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ email, password }),
+    body: JSON.stringify({ nome, email, senha }),
   })
     .then(response => response.json())
-    .then(data => {
+    /*.then(data => {
       // Manipula a resposta do back-end
       if (data.token) {
         // Registro bem-sucedido
@@ -28,6 +28,6 @@ function submitForm() {
         // Manipula erros de registro
         alert('Erro no registro. Verifique suas informações.');
       }
-    })
+    })*/
     .catch(error => console.error('Erro:', error));
 }
